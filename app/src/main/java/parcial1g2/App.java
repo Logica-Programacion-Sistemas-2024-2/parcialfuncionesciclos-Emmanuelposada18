c032fe9c9189ee9e6c69cf477a3ae4baca1077d2
Emmanuelposada18 <emanuelposada16@gmail.com>
2024-08-23 13:53:07 -0500

    Update App.java

--- app/src/main/java/parcial1g2/App.java
+++ app/src/main/java/parcial1g2/App.java
@@ -1,27 +1,69 @@
+package parcial1;
 
-package parcial1g2;
+import java.util.Scanner;
 
+public class parcial1 {
 
-public class App {
-    
-    public static void main(String[] args) {
-        
-        // Logica de solución
-
+    private static double obtenertasaRodamiento(int decada) {
+        switch (decada) {
+            case 90:
+                return 0.0409;
+            case 0:
+                return 0.0434;
+            case 10:
+                return 0.0493;
+            case 20:
+                return 0.0568;
+            default:
+                return 0;
+        }
     }
 
-    /*  Implemente una funcion llamada calcularTotalImpuestos que cumpla con las siguientes caracteristicas
-        * Recibe: 
-            - Una cadena de caracteres que representa la decada (70, 80, 90, 00, 10, 20, Otra)
-            - Un entero que representa avaluo del vehiculo
-        * Retorna:
-            - Un entero representando el valor total a pagar de los impuestos
-        * Controlar errores retornando -1
-    */
-    //------------------------------------------------------------------
-    
+    private static double obtenertasaSeguro(int decada) {
+        switch (decada) {
+            case 90:
+                return 0.00816;
+            case 0:
+                return 0.00798;
+            case 10:
+                return 0.00712;
+            case 20:
+                return 0.00699;
+            default:
+                return 0;
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    //------------------------------------------------------------------
+        int cantidadVehiculos = 0;
+        int decada = 0;
+        int avaluo = 0;
+
+        System.out.println("Agregue la cantidad de vehiculos");
+        cantidadVehiculos = sc.nextInt();
+
+        for (int i = 1; i <= cantidadVehiculos; i++) {
+
+            System.out.print("Ingrese el avalúo del vehículo: ");
+            avaluo = sc.nextInt();
+
+            System.out.print("Ingrese la década del modelo del vehículo (90, 00, 10, 20): ");
+            decada = sc.nextInt();
+
+            double tasaRodamiento = obtenertasaRodamiento(decada);
+            double tasaSeguro = obtenertasaSeguro(decada);
+            double impuestoRodamiento = avaluo * tasaRodamiento;
+            double seguro = avaluo * tasaSeguro;
+            double totalAPagar = impuestoRodamiento + seguro;
+
+            System.out.println("Vehículo " + cantidadVehiculos);
+            System.out.println("Avalúo: " + avaluo);
+            System.out.println("Década: " + decada);
+            System.out.println("Total A Pagar: " + totalAPagar);
+        }
+        sc.close();
+    }
 
 }
